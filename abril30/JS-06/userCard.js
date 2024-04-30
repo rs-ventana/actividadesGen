959e0a3fbda3b90ae5ad98202ec89202183411d8
rs-ventana <ruac.rs@outlook.com>
2024-04-30 16:44:20 -0600

    detalles

--- abril30/JS-06/userCard.js
+++ abril30/JS-06/userCard.js
@@ -7,7 +7,8 @@
  */
 const CARDS_CONTAINER = document.querySelector(".card-container");
 
-const USER_ONE = {
+const USERS = [
+    {
     id: 1,
     username: "booklover123",
     desc: "Avid reader and coffee enthusiast.",
@@ -19,9 +20,8 @@ const USER_ONE = {
             "Pride and Prejudice"
         ],
     },
-};
-
-const USER_TWO = {
+},
+{
     id: 2,
     username: "literature_junkie",
     desc: "Poetry lover and amateur writer.",
@@ -32,11 +32,11 @@ const USER_TWO = {
         "Beloved"
     ],
     },
-};
-
+},
+];
 
 // crear secciones
-function userSection(twoUsers){ // función para que se aplique a los users 
+function userSection(user){ // función para que se aplique a los users 
 const card = document.createElement("div");
 const name_section = document.createElement ("h3");
 const desc_section = document.createElement ("p");
@@ -49,16 +49,21 @@ const booklist= twoUsers.fav_books.books.map((e) =>{
     return item;
 });
 
-console.log(booklist);
+// console.log(booklist);
 
 // crear tarjeta vamos a usar el objeto,
-name_section.textContent = twoUsers.username;
-desc_section.textContent = twoUsers.desc;
-age_section.textContent = twoUsers.age;
+name_section.textContent = user.username;
+desc_section.textContent = user.desc;
+age_section.textContent = user.age;
 book_section.append(...booklist); // append permite varios nodos
 
 card.append(name_section, desc_section, age_section, book_section); 
-
+return card;
 }
-// por el momento no funciona
+
+
+manyUsers.array.forEach(user => {
+    const profileUser = userSection(user);
+    tarjetaObj(profileUser);
+});
 
