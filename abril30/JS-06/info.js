971fb8c5bdda1c97182eef2f88e46eda155667d7
rs-ventana <ruac.rs@outlook.com>
2024-04-30 16:26:24 -0600

    Avance, todavia no funciona

---
+++ abril30/JS-06/info.js
@@ -0,0 +1,21 @@
+// información de los usuarios
+const users = [
+    {
+      id: 1,
+      username: "booklover123",
+      description: "Avid reader and coffee enthusiast.",
+      age: 29,
+      fav_books: {
+        books: ["To Kill a Mockingbird", "1984", "Pride and Prejudice"]
+      }
+    },
+    {
+      id: 2,
+      username: "literature_junkie",
+      description: "Poetry lover and amateur writer.",
+      age: 35,
+      fav_books: {
+        books: ["The Great Gatsby", "Invisible Man", "Beloved"]
+      }
+    }
+  ];
