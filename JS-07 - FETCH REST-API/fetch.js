1d4fd2152fb865c67a63c2ccdc7cc83c87f9b44b
rs-ventana <ruac.rs@outlook.com>
2024-05-09 13:29:32 -0600

    Ponerme al día

--- JS-07 - FETCH REST-API/fetch.js
+++ JS-07 - FETCH REST-API/fetch.js
@@ -2,7 +2,7 @@
 //
 // POST
 
-const BASE_URL = 'https://pokeapi.co/api/v2/';
+BASE_URL = 'https://pokeapi.co/api/v2/';
 
 // Fetch no async
 /*
@@ -10,8 +10,8 @@ fetch(BASE_URL + 'pokemon/ditto')
     .then(res => res.json())
     .then(data => console.log(data));
 */
-// fetch async
 
+/// fetch async
 const fetchPokemon = async (pokemon) => {
     try {
         const response = await fetch(`${BASE_URL}pokemon/${pokemon}`);
@@ -20,56 +20,55 @@ const fetchPokemon = async (pokemon) => {
     } catch (err) {
         console.error(err);
     }
+} // funciona
+
+
+/// Obtener pokemon
+const savePokemon = (pokemon) => { // convertir en JSON y guardarlo
+    localStorage.setItem('currentPokeData', JSON.stringify(pokemon));
 }
 
-// guardar el pokemon
-const savePokemon = (pokemon) => {
-    localStorage.setItem('currentPokeData', JSON.stringify(wartotle));
-
-// Obtener pokemon
 document.getElementById('get-btn')
     .addEventListener('click', async () => {
         const text = document.getElementById('poke-name').value.toLowerCase();
         const pokemon = await fetchPokemon(text);
-        savePokemon(pokemon); // revisar
-        console.log(pokemon.name); 
+        savePokemon(pokemon); 
+        console.log(pokemon.name);
     })
-
-
-/** 
-// obtener el anterior
-document.getElementById('previous-btn').addEventListener('click', async () => {
-    const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
-    const newId = Math.max(1, currentPokeId - 1);
-    const pokemon = await fetchPokemon(newId);
-    savePokemonToLocalStorage(pokemon); // Guardar el Pokémon en localStorage
-    console.log(pokemon.name);
-})
+// por ahora, todo bien 
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const storedData = localStorage.getItem('currentPokeData');
-    if (storedData) {
-        const pokemon = JSON.parse(storedData);
+    const storedId = localStorage.getItem('currentPokeData');
+    if(storedPokemon){ //agregar parse
+        const pokemon = JSON.parse(storedPokemon);
         console.log(pokemon.name);
     } else {
         const storedId = localStorage.getItem('currentPokeId');
         const initialId = storedId ? parseInt(storedId) : 1;
         const pokemon = await fetchPokemon(initialId);
-        savePokemonToLocalStorage(pokemon); // Guardar el Pokémon en localStorage
+        savePokemon(pokemon); 
         console.log(pokemon.name);
     }
 })
 
-// obtener el siguiente
 
-document.getElementById('next-btn')
-    .addEventListener('click', async () => {
-        const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
-        const newId = currentPokeId + 1;
-        const pokemon = await fetchPokemon(newId);
-        console.log(pokemon);
-    })
+/// obtener el anterior
+document.getElementById('previous-btn').addEventListener('click', async () => {
+    const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
+    const newId = Math.max(1, currentPokeId - 1);
+    const pokemon = await fetchPokemon(newId);
+    savePokemon(pokemon);
+    console.log(pokemon.name);
 
+
+/// obtener el siguiente
+document.getElementById('next-btn').addEventListener('click', async () => {
+    const currentPokeId = parseInt(localStorage.getItem('currentPokeId'));
+    const newId = currentPokeId + 1;
+    const pokemon = await fetchPokemon(newId);
+    savePokemon(pokemon); 
+    console.log(pokemon.name);
+})
 
 
 ////////////////// POST
@@ -88,13 +87,9 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 }).then(res => res.json())
     .then(json => console.log(json))
 
-
 /////////////////// EJERCICIOS
 //- Arreglar el pokemon en localStorage
 // - Manipular el DOM y agregar una tarjeta del pokemon.
 // - El tamaño e info de la tarjeta es a consideración personal.
 // - La tarjeta debe mantenerse en la pantalla.
-// - La info -> LocalStorage -> Fetch
-
-
- */
+// - La info -> LocalStorage -> Fetch // 
